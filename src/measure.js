07051822eb83867c7a9bbf8c23be60de6a66024b
Michael Enion <caewok_2000@yahoo.com>
2021-08-15 15:20:29 -0700

    Revert "Avoid recalculating edges when using libRuler"
    This reverts commit 159d7dc1d204442db71113595675499dba900917.

--- src/measure.js
+++ src/measure.js
@@ -139,9 +139,7 @@ function measureDistancesHex(segments, options) {
 function measureDistancesGridless(segments, options) {
 	const costFunction = options.costFunction;
 
-  // avoid recalculating the edges for each segment when libRuler is active
-	const terrainEdges = game.modules.get('libruler')?.active ? this.ruler.getFlag("terrain-ruler", "terrainEdges") : collectTerrainEdges();
-	console.log(`terrain-ruler|terrainEdges`, terrainEdges);
+	const terrainEdges = collectTerrainEdges();
 	if (CONFIG.debug.terrainRuler)
 		debugEdges(terrainEdges);
 
